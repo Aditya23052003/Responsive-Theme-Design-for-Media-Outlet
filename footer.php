82c7133ae0b3ce67c8eb37be3131e7f90e45d0c8
Aditya Jadhav <98929144+Aditya23052003@users.noreply.github.com>
2024-05-17 23:15:19 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All Rights Reserved.</p>
+    </footer>
+
+    <?php wp_footer(); ?>
+    <script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
+</body>
+</html>
